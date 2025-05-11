af2642729f788bd7bd4073bae4ef67a209214379
Ilya Butakov <gabellaoff@yandex.ru>
2025-05-11 15:35:04 +0500

    fixed user deletion

--- src/application/use_cases/users/delete_user.py
+++ src/application/use_cases/users/delete_user.py
@@ -13,7 +13,7 @@ class UserDeleteUseCase(UseCase):
 
     async def execute(self, user_id: int) -> bool:
         async with self._uow(autocommit=True):
-            if await self._uow.users.exists(user_id):
+            if await self._uow.users.exists(id=user_id):
                 await self._uow.users.delete_by_id(user_id)
             else:
                 raise APIException(code=404, message=f"Пользователь с id '{user_id}' не существует")
